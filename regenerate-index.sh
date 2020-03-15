fbe247f3f30caa64d2848a88aeea27d007999419
root <root@optics1.fyre.ibm.com>
2020-03-15 05:29:44 +0000

    update bookinfo repo

--- regenerate-index.sh
+++ regenerate-index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd repo/incubator
-helm repo index --url https://raw.githubusercontent.com/dymaczew/charts/master/repo/incubator/ .
+helm repo index --url https://raw.githubusercontent.com/xiaospider/charts/master/repo/incubator/ .
